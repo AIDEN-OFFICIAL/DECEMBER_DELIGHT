a866d66cd59ae12f4bd034e60867187ba2946858
Aiden <aidencterrence5@gmaail.com>
2024-10-15 09:15:45 +0530

    second commit

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const app = express();
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.set("views engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
