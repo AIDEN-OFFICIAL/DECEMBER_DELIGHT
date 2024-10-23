005ee704c17cd593d3e2ef621e5f80411eb274e2
Aiden <aidencterrence5@gmaail.com>
2024-10-23 23:58:44 +0530

    the user management in admin is completed , with every functionalities and the category also made , now moving on to the category functionality

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,29 +1,45 @@
 const User = require("../models/userSchema");
 
-const userAuth = (req,res,next) => {
+const userAuth = (req, res, next) => {
     if (req.session.user) {
         User.findById(req.session.user)
-    .then(data => {
-        if (data && !data.isBlocked) {
-            next();
-        } else {
-            res.redirect("/login")
-        }
-    })
-        .catch({ error=>{
-            console.log("error in user auth middleware");
-            res.status(500).send("Internal Server error");
-        }
-    })
-
+            .then(data => {
+                if (data && !data.isBlocked) {
+                    next();
+                } else {
+                    res.redirect("/login")
+                }
+            })
+            .catch(error => {
+                console.log("error in user auth middleware");
+                res.status(500).send("Internal Server error");
+            })
+    }
+    else {
+        res.redirect("/login")
+    }
 }
 
 const adminAuth = (req, res, next) => {
-    if (!req.session.admin) {
-      return res.redirect('/admin/login');
+    if (req.session.user) {
+        User.findById(req.session.user)
+            .then(user => {
+                if (user && user.isAdmin) {
+                    next(); 
+                } else {
+                    res.redirect("/admin"); 
+                }
+            })
+            .catch(error => {
+                console.log("Error in admin auth middleware:", error);
+                res.status(500).send("Internal Server error");
+            });
+    } else {
+        res.redirect("/admin");
     }
-    next();
-  };
-  
-  module.exports = adminAuth;
-  
+};
+
+module.exports = {
+    userAuth,
+    adminAuth,
+};
