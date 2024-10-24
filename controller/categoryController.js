7a0fa07ebc9658b6584d7590b1e68ccfa7a4b9e6
Aiden <aidencterrence5@gmaail.com>
2024-10-24 11:55:46 +0530

    new category adding functionality is set , and the listing and unlisting of categoris is also done

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -1,10 +1,11 @@
 const Category = require('../models/categorySchema');
+const Product = require('../models/productSchema');
 const mongoose = require('mongoose');
 
 const categoryInfo = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
-    const limit = 2;
+    const limit = 3;
     const skip = (page - 1) * limit;
 
     const categoryData = await Category.find({})
@@ -40,7 +41,103 @@ const addCategory = async (req, res) => {
     return res.status(500).json({ error: 'Internal server error' });
   }
 };
+
+const addCategoryOffer = async (req, res) => {
+  try {
+    const percentage = parseInt(req.body.percentage);
+    const categoryId = req.body.categoryId;
+    const category = await Category.findById(categoryId);
+    if (!category) {
+      return res
+        .status(404)
+        .json({ status: false, message: 'Category not found' });
+    }
+    const products = await Product.find({ category: category._id });
+    console.log(products);
+
+    const hasProductOffer = products.some(
+      (Product) => product.productOffer > percentage
+    );
+    if (hasProductOffer) {
+      return res.json({
+        status: false,
+        message: 'Product within this category already has product offers',
+      });
+    }
+    await Category.updateOne(
+      { _id: categoryId },
+      { $set: { CategoryOffer: percentage } }
+    );
+    console.log(products);
+
+    for (const product of products) {
+      product.productOffer = 0;
+      product.salePrice = product.regularPrice;
+      await product.save();
+    }
+    res.json({ status: true });
+  } catch (error) {
+    res.status(500).json({ status: false, message: 'Internal server error' });
+  }
+};
+
+const removeCategoryOffer = async (req, res) => {
+  try {
+    const categoryId = req.body.categoryId;
+
+    const category = await Category.findById(categoryId);
+
+    if (!category) {
+      return res
+        .status(404)
+        .json({ status: false, message: 'Category not found' });
+    }
+
+    const percentage = category.CategoryOffer;
+    const products = await Product.find({ category: category._id });
+
+    if (products.length > 0) {
+      for (const product of products) {
+        product.salePrice += Math.floor(
+          product.regularPrice * (percentage / 100)
+        );
+        product.productOffer = 0;
+        await product.save();
+      }
+    }
+
+    category.CategoryOffer = 0;
+    await category.save();
+    res.json({ status: true });
+  } catch (error) {
+    res.status(500).json({ status: false, message: 'Iternal server error' });
+  }
+};
+
+const getListCategory = async (req, res) => {
+  try {
+    let id = req.query.id;
+    await Category.updateOne({ _id: id }, { $set: { isListed: false } });
+    res.redirect('/admin/category');
+  } catch (error) {
+    res.redirect('/pageError');
+  }
+};
+
+const getUnlistCategory = async (req, res) => {
+  try {
+    let id = req.query.id;
+    await Category.updateOne({ _id: id }, { $set: { isListed: true } });
+    res.redirect('/admin/category');
+  } catch (error) {
+    res.redirect('/pageError');
+  }
+};
 module.exports = {
   categoryInfo,
   addCategory,
+  addCategoryOffer,
+  removeCategoryOffer,
+  getListCategory,
+  getUnlistCategory,
 };
