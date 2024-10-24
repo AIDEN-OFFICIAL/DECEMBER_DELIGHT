2514594e3273a68f5dd8542fb8d45e72a49cacb9
Aiden <aidencterrence5@gmaail.com>
2024-10-24 18:11:38 +0530

    category is complete added edit feature now moving on to the product page

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -133,6 +133,39 @@ const getUnlistCategory = async (req, res) => {
     res.redirect('/pageError');
   }
 };
+const getEditCategory= async (req,res)=>{
+    try {
+        let id=req.query.id;
+       const category= await Category.findOne({_id:id});
+        res.render("editCategory",{category:category});
+    } catch (error) {
+        res.redirect('/pageerror')
+    }
+}
+const editCategory= async(req,res)=>{
+    try {
+        let id=req.params.id;
+       const {categoryName,description}=req.body;
+       const existingCategory= await Category.findOne({name:categoryName});
+       if(existingCategory){
+        return res.status(400).json({error:"Category exists, please choose another name"});
+       }
+       const updateCategory=await Category.findByIdAndUpdate(id,{
+        name:categoryName,
+        description:description, 
+       }, { new: true });
+        
+       if(updateCategory){
+        return res.status(200).json({ success: "Category updated successfully" });
+    }else{
+        res.status(404).json({error:"Category not found"})
+       }
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error:"Internal server error"})
+    }
+}
 module.exports = {
   categoryInfo,
   addCategory,
@@ -140,4 +173,6 @@ module.exports = {
   removeCategoryOffer,
   getListCategory,
   getUnlistCategory,
+  getEditCategory,
+  editCategory,
 };
